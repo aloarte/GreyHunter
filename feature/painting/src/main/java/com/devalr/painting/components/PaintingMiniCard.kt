068d8b127e32666392bd044140b458165a965b14
Álvaro <aloarter@gmail.com>
2026-01-14 12:01:42 +0100

    Removed commented code

--- feature/painting/src/main/java/com/devalr/painting/components/PaintingMiniCard.kt
+++ feature/painting/src/main/java/com/devalr/painting/components/PaintingMiniCard.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -15,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,20 +28,6 @@ import com.devalr.framework.theme.GreyHunterTheme
 
 @Composable
 fun PaintingMiniCard(miniature: MiniatureBo) {
-    /*Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        GHText(text = miniature.name, type = TextType.LabelL, textColor = Color.White)
-        Spacer(modifier = Modifier.height(10.dp))
-        GHImage(
-            imageUri = miniature.imageUri,
-            size = calculateHeight(getScreenSize())
-        )
-    }*/
-
     Box(
         modifier = Modifier
             .size(calculateHeight(getScreenSize()))
@@ -103,4 +87,3 @@ private fun PaintingMiniCardDarkModePreview() {
         PaintingMiniCard(miniature = technomancer)
     }
 }
-
