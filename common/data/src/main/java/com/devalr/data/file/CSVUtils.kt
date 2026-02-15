6e235484ef0cdd62e2755404e73119fbb079e484
Álvaro <aloarter@gmail.com>
2026-02-15 22:38:59 +0100

    Test for file management added

--- common/data/src/main/java/com/devalr/data/file/CSVUtils.kt
+++ common/data/src/main/java/com/devalr/data/file/CSVUtils.kt
@@ -14,18 +14,33 @@ fun String?.escapeCsv(): String {
 fun parseCsvLine(line: String): List<String> {
     val result = mutableListOf<String>()
     val sb = StringBuilder()
+    var i = 0
     var inQuotes = false
 
-    for (char in line) {
+    while (i < line.length) {
+        val char = line[i]
+
         when {
-            char == '"' -> inQuotes = !inQuotes
+            char == '"' -> {
+                if (inQuotes && i + 1 < line.length && line[i + 1] == '"') {
+                    sb.append('"')
+                    i++
+                } else {
+                    inQuotes = !inQuotes
+                }
+            }
+
             char == ',' && !inQuotes -> {
                 result += sb.toString()
                 sb.clear()
             }
+
             else -> sb.append(char)
         }
+
+        i++
     }
+
     result += sb.toString()
     return result
 }
